01144f9a9bff30b17a1785c30d1a27310958d35b
Aviya Litman <aviyalitman@github.com>
2024-06-14 17:22:21 -0400

    cleaned up gene sets code

--- GenomicAnalyses/utils.py
+++ GenomicAnalyses/utils.py
@@ -30,13 +30,8 @@ def load_dnvs():
     # parse out extra information
     dnvs['am_class'] = dnvs['Extra'].str.extract(r'am_class=(.*?);')
     dnvs['am_class'] = dnvs['am_class'].apply(lambda x: 1 if x in ['likely_pathogenic'] else 0)
-    dnvs['am_pathogenicity'] = dnvs['Extra'].str.extract(r'am_pathogenicity=([\d.]+)').astype(float)
-    dnvs['am_pathogenicity'] = dnvs['am_pathogenicity'].apply(lambda x: 1 if x>=0.9 else 0)
     dnvs['LoF'] = dnvs['Extra'].str.extract(r'LoF=(.*?);')
     dnvs['LoF'] = dnvs['LoF'].apply(lambda x: 1 if x == 'HC' else 0)
-    dnvs['LoF_flags'] = dnvs['Extra'].str.extract(r'LoF_flags=(.*?);')
-    dnvs['LoF_flags'] = dnvs['LoF_flags'].fillna(1)
-    dnvs['LoF_flags'] = dnvs['LoF_flags'].apply(lambda x: 1 if x in ['SINGLE_EXON',1] else 0)
     dnvs = dnvs.drop('Extra', axis=1)
 
     with open('data/SPID_to_vars.pkl', 'rb') as f:
@@ -57,7 +52,7 @@ def load_dnvs():
     dnvs_sibs = dnvs[dnvs['asd'] == 1]
     dnvs_pro = dnvs[dnvs['asd'] == 2]
 
-    gfmm_labels = pd.read_csv('../PhenotypeClasses/data/SPARK_5392_ninit_cohort_GFMM_labeled.csv', index_col=False, header=0) # 5391 probands
+    gfmm_labels = pd.read_csv('../PhenotypeClasses/data/SPARK_5392_ninit_cohort_GFMM_labeled.csv', index_col=False, header=0)
     gfmm_labels = gfmm_labels.rename(columns={'subject_sp_id': 'spid'})
     gfmm_labels = gfmm_labels[['spid', 'mixed_pred']]
     spid_to_class = dict(zip(gfmm_labels['spid'], gfmm_labels['mixed_pred']))
@@ -90,9 +85,6 @@ def get_gene_sets():
     satterstrom = pd.read_csv('gene_sets/satterstrom_2020_102_ASD_genes.csv', header=0, index_col=False).rename({'gene': 'name'}, axis='columns')
     
     sfari_genes1 = list(sfari_genes[sfari_genes['gene-score'] == 1]['name'])
-    sfari_genes2 = list(sfari_genes[sfari_genes['gene-score'] == 2]['name'])
-    sfari_syndromic = list(sfari_genes[sfari_genes['syndromic'] == 1]['name'])
-    sfari_genes = list(sfari_genes['name'])
     lof_genes = list(lof_genes['name'])
     fmrp_genes = list(fmrp_genes['name'])
     asd_risk_genes = list(asd_risk_genes['name'])
@@ -102,14 +94,8 @@ def get_gene_sets():
     all_genes.columns = ['chr', 'start', 'end', 'gene', 'name', 'strand']
     all_genes = list(all_genes['name'])
 
-    pli_table = pd.read_csv('gene_sets/pLI_table.txt', sep='\t', index_col=False)
-    # get genes with pLI >= 0.995
-    pli_genes_high = pli_table[pli_table['pLI'] >= 0.995]['gene'].tolist()
-    # get genes with pli 0.5-0.995
-    pli_genes_low = pli_table[(pli_table['pLI'] < 0.995) & (pli_table['pLI'] >= 0.5)]['gene'].tolist()
-
-    gene_list = [all_genes, lof_genes, fmrp_genes, asd_risk_genes, brain_expressed_genes, satterstrom, sfari_genes, sfari_genes1, sfari_genes2, sfari_syndromic, pli_genes_high, pli_genes_low]
-    gene_list_names = ['all_genes', 'lof_genes', 'fmrp_genes', 'asd_risk_genes','brain_expressed_genes', 'asd_coexpression_networks', 'psd_genes', 'satterstrom', 'sfari_genes', 'sfari_genes1', 'sfari_genes2', 'sfari_syndromic', 'pli_genes_highest', 'pli_genes_medium', 'ddg2p', 'liver_genes']
+    gene_list = [all_genes, lof_genes, fmrp_genes, asd_risk_genes, brain_expressed_genes, satterstrom, sfari_genes1]
+    gene_list_names = ['all_genes', 'lof_genes', 'fmrp_genes', 'asd_risk_genes', 'brain_expressed_genes', 'satterstrom', 'sfari_genes1']
 
     return gene_list, gene_list_names
 
