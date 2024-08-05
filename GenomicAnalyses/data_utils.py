d08ff91c8a83f0850ee0fed36a8635e2c102c86c
Aviya Litman <aviyalitman@github.com>
2024-08-05 17:01:42 -0400

    added hail table rare variant filtering

--- GenomicAnalyses/data_utils.py
+++ GenomicAnalyses/data_utils.py
@@ -120,7 +120,7 @@ def process_DNVs():
     print(f'Mean: {mean}')
     print(f'SD: {sd}')
     threshold = mean + 3*sd
-    # FILTER: remove SPIDs with more than 3SD DNVs
+    # FILTER: remove SPIDs with more than 3SD DNVs above the mean
     spid_to_count = {k: v for k, v in spid_to_count.items() if v <= threshold}
     SPID_to_vars = {k: v for k, v in SPID_to_vars.items() if k in spid_to_count.keys()}
 
@@ -151,55 +151,69 @@ def process_DNVs():
         rick.dump(SPID_to_vars, f2)
 
 
+def fetch_rare_vars_with_hail():
+    hl.init()
+
+    gnomad_v4 = hl.read_table('gs://gcp-public-data--gnomad/release/4.1/ht/exomes/gnomad.exomes.v4.1.sites.ht')
+
+    rare_variants_ht = gnomad_v4.filter(gnomad_v4.freq[0].AF < 0.01)
+
+    rare_variants_ht = rare_variants_ht.annotate(
+        variant_id = hl.str("chr") + hl.str(rare_variants_ht.locus.contig) + "_" + 
+                    hl.str(rare_variants_ht.locus.position) + "_" + 
+                    hl.str(rare_variants_ht.alleles[0]) + "_" + 
+                    hl.str(rare_variants_ht.alleles[1])
+    )
+
+    rare_variants_ht = rare_variants_ht.select('variant_id', AF=rare_variants_ht.freq[0].AF)
+    rare_variants_ht = rare_variants_ht.key_by('variant_id')
+
+    output_tsv_path = 'data/rare_variants.tsv.bgz'
+    rare_variants_ht.export(output_tsv_path)
+    hl.stop()
+
+
 def combine_inherited_vep_files():
-    data_dir = 'data/inherited_vep_predictions_plugins_filtered/' # filtered repeats + centromeres
-    files = [f for f in os.listdir(data_dir) if f.endswith('.vcf')]
+    rare_variants_df = pd.read_csv('data/rare_variants.tsv.bgz', sep='\t', compression='gzip')
+    variant_to_af = dict(zip(rare_variants_df['variant_id'], rare_variants_df['AF']))
+
+    directory = 'inherited_vep_predictions_plugins_filtered/' # filtered repeats + centromeres
+    files = [f for f in os.listdir(directory) if f.endswith('.vcf')]
     spids = [f.split('.')[0] for f in files]
 
-    spid_to_num_ptvs = {} # dictionary of spid to number of PTVs
-    spid_to_num_missense = {} # dictionary of spid to number of missense variants
+    spid_to_num_ptvs = {}
+    spid_to_num_missense = {}
 
     gene_sets, gene_set_names = get_gene_sets()
     consequences_lof = ['stop_gained', 'frameshift_variant', 'splice_acceptor_variant', 'splice_donor_variant', 'start_lost', 'stop_lost', 'transcript_ablation']
     consequences_missense = ['missense_variant', 'inframe_deletion', 'inframe_insertion', 'protein_altering_variant']
 
-    gfmm_labels = pd.read_csv('../PhenotypeClasses/data/SPARK_5392_ninit_cohort_GFMM_labeled.csv', index_col=False, header=0)
-    gfmm_ids = gfmm_labels['subject_sp_id'].tolist()
+    gfmm_labels = pd.read_csv('../PhenotypeValidations/data/SPARK_SSC_combined_cohort_phenotypes.csv', index_col=False, header=0)
+    gfmm_ids = gfmm_labels.iloc[:, 0].tolist()
 
-    sibling_list = '../PhenotypeClasses/data/WES_5392_siblings_spids.txt'
+    sibling_list = '../PhenotypeValidations/data/WES_5392_siblings_spids.txt'
     sibling_list = pd.read_csv(sibling_list, sep='\t', header=None)
     sibling_list.columns = ['spid']
     sibling_list = sibling_list['spid'].tolist()
 
     gfmm_ids = gfmm_ids + sibling_list
 
-    af = load_AF()
-    var_to_af = dict(zip(af['id'], af['af']))
-
-    counts_pros = []
-    counts_sibs = []
-
     ensembl_to_gene = dict(zip(ENSEMBL_TO_GENE_NAME['Gene'], ENSEMBL_TO_GENE_NAME['name']))
-    gene_to_spid_counts = defaultdict(dict)
     for i in range(len(files)):
-        if (spids[i] not in gfmm_ids) and (spids[i] not in sibling_list):
+        if spids[i] not in gfmm_ids:
             continue
         cols = ['Uploaded_variation', 'Location', 'Allele', 'Gene', 'Feature', 'Feature_type', 'Consequence', 'cDNA_position', 'CDS_position', 'Protein_position', 'Amino_acids', 'Codons', 'Existing_variation', 'Extra']
-        df = pd.read_csv(dir + files[i], sep='\t', comment='#', header=None, names=cols, index_col=False)
-        df = df[['Uploaded_variation', 'Gene', 'Consequence', 'Extra']]
-        df['af'] = df['Uploaded_variation'].map(var_to_af)
-        df = df[df['af'] <= 0.01] # filter to rare variants (af<0.01)
+        df = pd.read_csv(directory + files[i], sep='\t', comment='#', header=None, names=cols, index_col=False)
         
-        # filter plugins LOFTEE, AM
+        df = df[['Uploaded_variation', 'Gene', 'Consequence', 'Extra']]
+        df['AF'] = df['Uploaded_variation'].map(variant_to_af)
+        df = df.dropna(subset=['AF']) # only keep rare variants (af<0.01 from gnomAD exomes)
+        
+        # filter PTVs and missense variants with plugins LOFTEE, AM
         df['am_class'] = df['Extra'].str.extract(r'am_class=(.*?);')
         df['am_class'] = df['am_class'].apply(lambda x: 1 if x in ['likely_pathogenic'] else 0)
-        df['am_pathogenicity'] = df['Extra'].str.extract(r'am_pathogenicity=([\d.]+)').astype(float)
-        df['am_pathogenicity'] = df['am_pathogenicity'].apply(lambda x: 1 if x>=0.9 else 0)
         df['LoF'] = df['Extra'].str.extract(r'LoF=(.*?);')
         df['LoF'] = df['LoF'].apply(lambda x: 1 if x == 'HC' else 0)
-        df['LoF_flags'] = df['Extra'].str.extract(r'LoF_flags=(.*?);')
-        df['LoF_flags'] = df['LoF_flags'].fillna(1) # no flag is good
-        df['LoF_flags'] = df['LoF_flags'].apply(lambda x: 1 if x in ['SINGLE_EXON',1] else 0)
         df = df.drop('Extra', axis=1)
 
         df['name'] = df['Gene'].map(ensembl_to_gene)
@@ -207,8 +221,8 @@ def combine_inherited_vep_files():
         ptv_counts = []
         missense_counts = []
         for gene_set in gene_sets:
-            num_ptvs = df[df['name'].isin(gene_set) & df['Consequence'].isin(consequences_lof) & (df['LoF'] == 1 == 1)].shape[0]
-            num_missense = df[df['name'].isin(gene_set) & df['Consequence'].isin(consequences_missense) & (df['am_class'] == 1)].shape[0]
+            num_ptvs = df[(df['name'].isin(gene_set)) & (df['Consequence'].isin(consequences_lof)) & (df['LoF'] == 1)].shape[0]
+            num_missense = df[(df['name'].isin(gene_set)) & (df['Consequence'].isin(consequences_missense)) & (df['am_class'] == 1)].shape[0]
             ptv_counts.append(num_ptvs)
             missense_counts.append(num_missense)
         spid_to_num_ptvs[spids[i]] = ptv_counts
