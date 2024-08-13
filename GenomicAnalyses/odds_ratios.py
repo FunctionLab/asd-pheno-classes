5c31383c05593318a96fc81e87f266dea7a735f0
Aviya Litman <aviyalitman@github.com>
2024-08-13 14:31:35 -0400

    odds ratios cleanup

--- GenomicAnalyses/odds_ratios.py
+++ GenomicAnalyses/odds_ratios.py
@@ -8,37 +8,47 @@ from statsmodels.stats.multitest import multipletests
 
 def compute_odds_ratios():
     plt.style.use('seaborn-v0_8-whitegrid')
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 6), sharey=True, gridspec_kw={'width_ratios': [2.3, 1]})
+    fig, (ax1, ax2) = plt.subplots(1, 2, 
+                                   figsize=(8, 6), 
+                                   sharey=True, 
+                                   gridspec_kw={'width_ratios': 
+                                                [2.3, 1]}
+                                   )
 
     dnvs_pro, dnvs_sibs, zero_pro, zero_sibs = load_dnvs()
-    consequences_missense = ['missense_variant', 'inframe_deletion', 'inframe_insertion', 'protein_altering_variant']
     consequences_benign = ['synonymous_variant']
-    consequences_lof = ['stop_gained', 'frameshift_variant', 'splice_acceptor_variant', 'splice_donor_variant', 'start_lost', 'stop_lost', 'transcript_ablation']
-    dnvs_pro['lof'] = dnvs_pro['Consequence'].apply(lambda x: 1 if x in consequences_lof else 0)
-    dnvs_sibs['lof'] = dnvs_sibs['Consequence'].apply(lambda x: 1 if x in consequences_lof else 0)
+    consequences_lof = ['stop_gained', 'frameshift_variant', 
+                        'splice_acceptor_variant', 'splice_donor_variant', 
+                        'start_lost', 'stop_lost', 'transcript_ablation']
+    dnvs_pro['lof'] = dnvs_pro['Consequence'].apply(
+        lambda x: 1 if x in consequences_lof else 0)
+    dnvs_sibs['lof'] = dnvs_sibs['Consequence'].apply(
+        lambda x: 1 if x in consequences_lof else 0)
     
     gene_sets, gene_set_names = get_gene_sets()    
     gene_set_of_interest = 'fmrp_genes'
     for i in range(len(gene_sets)):
         if gene_set_names[i] == gene_set_of_interest:
             selected_gene_set = gene_sets[i]
-            dnvs_pro[gene_set_names[i]] = dnvs_pro['name'].apply(lambda x: 1 if x in gene_sets[i] else 0)
-            dnvs_sibs[gene_set_names[i]] = dnvs_sibs['name'].apply(lambda x: 1 if x in gene_sets[i] else 0)
+            dnvs_pro[gene_set_names[i]] = dnvs_pro['name'].apply(
+                lambda x: 1 if x in gene_sets[i] else 0)
+            dnvs_sibs[gene_set_names[i]] = dnvs_sibs['name'].apply(
+                lambda x: 1 if x in gene_sets[i] else 0)
 
-    num_class0 = dnvs_pro[dnvs_pro['class'] == 0]['spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 0]['spid'].nunique()
-    num_class1 = dnvs_pro[dnvs_pro['class'] == 1]['spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 1]['spid'].nunique()
-    num_class2 = dnvs_pro[dnvs_pro['class'] == 2]['spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 2]['spid'].nunique()
-    num_class3 = dnvs_pro[dnvs_pro['class'] == 3]['spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 3]['spid'].nunique()
+    num_class0 = dnvs_pro[dnvs_pro['class'] == 0][
+        'spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 0]['spid'].nunique()
+    num_class1 = dnvs_pro[dnvs_pro['class'] == 1][
+        'spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 1]['spid'].nunique()
+    num_class2 = dnvs_pro[dnvs_pro['class'] == 2][
+        'spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 2]['spid'].nunique()
+    num_class3 = dnvs_pro[dnvs_pro['class'] == 3][
+        'spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 3]['spid'].nunique()
     num_sibs = dnvs_sibs['spid'].nunique() + zero_sibs['spid'].nunique()
 
     dnvs_pro['lof_final_consequence'] = dnvs_pro['lof'] * dnvs_pro['LoF']
     dnvs_sibs['lof_final_consequence'] = dnvs_sibs['lof'] * dnvs_sibs['LoF']
 
     odds_ratios = defaultdict(list)
-    class_to_gene_set = {}
-    class_to_gene_set_log_fold_change = defaultdict(dict)
-    num_mutations = []
-    props = []
     pvals_lof = []
     pvals_syn = []
     for class_id, class_count in zip([0,1,2,3], [num_class0, num_class1, num_class2, num_class3]):
@@ -46,7 +56,8 @@ def compute_odds_ratios():
         gene_vars_for_class = gene_vars[gene_vars['class'] == class_id]
         gene_vars_sibs = dnvs_sibs[dnvs_sibs['name'].isin(selected_gene_set)]
 
-        lof_gene_vars_for_class = gene_vars_for_class[gene_vars_for_class['lof_final_consequence'] == 1]
+        lof_gene_vars_for_class = gene_vars_for_class[
+            gene_vars_for_class['lof_final_consequence'] == 1]
         lof_case_variant_present_count = lof_gene_vars_for_class['spid'].nunique()
         lof_gene_vars_sibs = gene_vars_sibs[gene_vars_sibs['lof_final_consequence'] == 1]
         lof_sibs_case_variant_present_count = lof_gene_vars_sibs['spid'].nunique()
@@ -71,7 +82,8 @@ def compute_odds_ratios():
     for i in range(len(odds_ratios_for_plotting[0])):
         x_values = [x + i * bar_width+0.075 for x in x_positions]
         y_values = [item[i] for item in odds_ratios_for_plotting]
-        ax2.bar(x_values, y_values, width=bar_width, label=f'Value {i+1}', color=colors[i])
+        ax2.bar(x_values, y_values, width=bar_width, 
+                label=f'Value {i+1}', color=colors[i])
 
     ax2.set_xticks([x + 2 * bar_width for x in x_positions])
     ax2.set_xticklabels(['dnLoF'], fontsize=18)
@@ -90,27 +102,38 @@ def compute_odds_ratios():
 
     # SECOND SUBPLOT
     dnvs_pro, dnvs_sibs, zero_pro, zero_sibs = load_dnvs()
-    consequences_missense = ['missense_variant', 'inframe_deletion', 'inframe_insertion', 'protein_altering_variant']
     consequences_benign = ['synonymous_variant']
-    consequences_lof = ['stop_gained', 'frameshift_variant', 'splice_acceptor_variant', 'splice_donor_variant', 'start_lost', 'stop_lost', 'transcript_ablation']
+    consequences_lof = ['stop_gained', 'frameshift_variant', 
+                        'splice_acceptor_variant', 'splice_donor_variant', 
+                        'start_lost', 'stop_lost', 'transcript_ablation']
 
-    dnvs_pro['synonymous'] = dnvs_pro['Consequence'].apply(lambda x: 1 if x in consequences_benign else 0)
-    dnvs_sibs['synonymous'] = dnvs_sibs['Consequence'].apply(lambda x: 1 if x in consequences_benign else 0)
-    dnvs_pro['lof'] = dnvs_pro['Consequence'].apply(lambda x: 1 if x in consequences_lof else 0)
-    dnvs_sibs['lof'] = dnvs_sibs['Consequence'].apply(lambda x: 1 if x in consequences_lof else 0)
+    dnvs_pro['synonymous'] = dnvs_pro['Consequence'].apply(
+        lambda x: 1 if x in consequences_benign else 0)
+    dnvs_sibs['synonymous'] = dnvs_sibs['Consequence'].apply(
+        lambda x: 1 if x in consequences_benign else 0)
+    dnvs_pro['lof'] = dnvs_pro['Consequence'].apply(
+        lambda x: 1 if x in consequences_lof else 0)
+    dnvs_sibs['lof'] = dnvs_sibs['Consequence'].apply(
+        lambda x: 1 if x in consequences_lof else 0)
     
     gene_sets, gene_set_names = get_gene_sets()    
     gene_set_of_interest = 'asd_risk_genes'
     for i in range(len(gene_sets)):
         if gene_set_names[i] == gene_set_of_interest:
             selected_gene_set = gene_sets[i]
-            dnvs_pro[gene_set_names[i]] = dnvs_pro['name'].apply(lambda x: 1 if x in gene_sets[i] else 0)
-            dnvs_sibs[gene_set_names[i]] = dnvs_sibs['name'].apply(lambda x: 1 if x in gene_sets[i] else 0)
+            dnvs_pro[gene_set_names[i]] = dnvs_pro['name'].apply(
+                lambda x: 1 if x in gene_sets[i] else 0)
+            dnvs_sibs[gene_set_names[i]] = dnvs_sibs['name'].apply(
+                lambda x: 1 if x in gene_sets[i] else 0)
 
-    num_class0 = dnvs_pro[dnvs_pro['class'] == 0]['spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 0]['spid'].nunique()
-    num_class1 = dnvs_pro[dnvs_pro['class'] == 1]['spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 1]['spid'].nunique()
-    num_class2 = dnvs_pro[dnvs_pro['class'] == 2]['spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 2]['spid'].nunique()
-    num_class3 = dnvs_pro[dnvs_pro['class'] == 3]['spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 3]['spid'].nunique()
+    num_class0 = dnvs_pro[dnvs_pro['class'] == 0][
+        'spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 0]['spid'].nunique()
+    num_class1 = dnvs_pro[dnvs_pro['class'] == 1][
+        'spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 1]['spid'].nunique()
+    num_class2 = dnvs_pro[dnvs_pro['class'] == 2][
+        'spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 2]['spid'].nunique()
+    num_class3 = dnvs_pro[dnvs_pro['class'] == 3][
+        'spid'].nunique() + zero_pro[zero_pro['mixed_pred'] == 3]['spid'].nunique()
     num_sibs = dnvs_sibs['spid'].nunique() + zero_sibs['spid'].nunique()
 
     dnvs_pro['syn_final_consequence'] = dnvs_pro['synonymous']
@@ -129,27 +152,32 @@ def compute_odds_ratios():
         gene_vars = dnvs_pro[dnvs_pro['name'].isin(selected_gene_set)]
         gene_vars_for_class = gene_vars[gene_vars['class'] == class_id]
         gene_vars_sibs = dnvs_sibs[dnvs_sibs['name'].isin(selected_gene_set)]
-
-        lof_gene_vars_for_class = gene_vars_for_class[gene_vars_for_class['lof_final_consequence'] == 1]
+        
+        # LoF
+        lof_gene_vars_for_class = gene_vars_for_class[
+            gene_vars_for_class['lof_final_consequence'] == 1]
         lof_case_variant_present_count = lof_gene_vars_for_class['spid'].nunique()
         lof_gene_vars_sibs = gene_vars_sibs[gene_vars_sibs['lof_final_consequence'] == 1]
         lof_sibs_case_variant_present_count = lof_gene_vars_sibs['spid'].nunique()
         lof_case_variant_absent_count = class_count - lof_case_variant_present_count
         lof_sibs_case_variant_absent_count = num_sibs - lof_sibs_case_variant_present_count
 
-        # syn
-        syn_gene_vars_for_class = gene_vars_for_class[gene_vars_for_class['syn_final_consequence'] == 1]
+        # Syn
+        syn_gene_vars_for_class = gene_vars_for_class[
+            gene_vars_for_class['syn_final_consequence'] == 1]
         syn_case_variant_present_count = syn_gene_vars_for_class['spid'].nunique()
         syn_gene_vars_sibs = gene_vars_sibs[gene_vars_sibs['syn_final_consequence'] == 1]
         syn_sibs_case_variant_present_count = syn_gene_vars_sibs['spid'].nunique()
         syn_case_variant_absent_count = class_count - syn_case_variant_present_count
         syn_sibs_case_variant_absent_count = num_sibs - syn_sibs_case_variant_present_count
         
+        # LoF odds ratio
         table_lof = [[lof_case_variant_present_count, lof_case_variant_absent_count],
                     [lof_sibs_case_variant_present_count, lof_sibs_case_variant_absent_count]]
         odds_ratio_lof, pval_lof = fisher_exact(table_lof)
         pvals_lof.append(pval_lof)
 
+        # Syn odds ratio
         table_syn = [[syn_case_variant_present_count, syn_case_variant_absent_count],
                     [syn_sibs_case_variant_present_count, syn_sibs_case_variant_absent_count]]
         odds_ratio_syn, pval_syn = fisher_exact(table_syn)
@@ -164,7 +192,7 @@ def compute_odds_ratios():
     print(corrected_lof)
     print(corrected_syn)
 
-    odds_ratios_for_plotting = [odds_ratios['lof'], odds_ratios['syn']] # odds_ratios['mis'],
+    odds_ratios_for_plotting = [odds_ratios['lof'], odds_ratios['syn']]
     bar_width = 0.15
     x_positions = range(len(odds_ratios_for_plotting))
 
@@ -172,7 +200,8 @@ def compute_odds_ratios():
     for i in range(len(odds_ratios_for_plotting[0])):
         x_values = [x + i * bar_width+0.075 for x in x_positions]
         y_values = [item[i] for item in odds_ratios_for_plotting]
-        ax1.bar(x_values, y_values, width=bar_width, label=f'Value {i+1}', color=colors[i])
+        ax1.bar(x_values, y_values, width=bar_width, 
+                label=f'Value {i+1}', color=colors[i])
 
     ax1.set_xticks([x + 2 * bar_width for x in x_positions])
     ax1.set_xticklabels(['dnLoF', 'dnSyn'], fontsize=18)
@@ -189,7 +218,11 @@ def compute_odds_ratios():
     ax1.grid(which='both', axis='y', color='gray', linestyle='-')
     ax1.grid(which='both', axis='x', linestyle='')
     plt.tight_layout()
-    plt.savefig('figures/WES_odds_ratios_figure.png', bbox_inches='tight', dpi=600)
+    plt.savefig(
+        'figures/WES_odds_ratios_figure.png', 
+        bbox_inches='tight', 
+        dpi=600
+        )
     plt.close()
 
 
