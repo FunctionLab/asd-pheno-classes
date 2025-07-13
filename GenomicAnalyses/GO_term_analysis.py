d40e27ea0e3bbf8226a2f581f3372b2e03df2faa
aviya <52050744+aviyalitman@users.noreply.github.com>
2025-07-13 14:12:56 -0400

    cleanup tested for readability

--- GenomicAnalyses/GO_term_analysis.py
+++ GenomicAnalyses/GO_term_analysis.py
@@ -21,7 +21,7 @@ def get_impacted_genes_per_class():
                         'start_lost', 'stop_lost', 'transcript_ablation']
 
     # annotate dnvs_pro and dnvs_sibs with consequence (binary)
-    dnvs_pro['consequence'] = dnvs_pro['Consequence'].apply(
+    dnvs_pro['consequence_missense'] = dnvs_pro['Consequence'].apply(
         lambda x: 1 if x in consequences_missense else 0)
     dnvs_pro['consequence_lof'] = dnvs_pro['Consequence'].apply(
         lambda x: 1 if x in consequences_lof else 0)
@@ -34,7 +34,7 @@ def get_impacted_genes_per_class():
         lambda x: 1 if x in gene_sets[all_genes_idx] else 0)
 
     # further subset variants based on LOFTEE and AlphaMissense
-    dnvs_pro['final_consequence'] = dnvs_pro['consequence'] * dnvs_pro['am_class']
+    dnvs_pro['final_consequence_mis'] = dnvs_pro['consequence_missense'] * dnvs_pro['am_class']
     dnvs_pro['final_consequence_lof'] = dnvs_pro['consequence_lof'] * dnvs_pro['LoF']
         
     class_to_gene_set = {}
@@ -43,7 +43,7 @@ def get_impacted_genes_per_class():
         gene_vars_for_class = gene_vars[gene_vars['class'] == class_id]
         
         mis_gene_vars_for_class = gene_vars_for_class[
-            gene_vars_for_class['final_consequence'] == 1]
+            gene_vars_for_class['final_consequence_mis'] == 1]
         lof_gene_vars_for_class = gene_vars_for_class[
             gene_vars_for_class['final_consequence_lof'] == 1]
 
@@ -52,8 +52,6 @@ def get_impacted_genes_per_class():
         genes.extend(lof_gene_vars_for_class['name'].unique())
        
         class_to_gene_set[class_id] = list(set(genes))
-        print(class_id, len(class_to_gene_set[class_id]))
-        print(class_to_gene_set[class_id])
 
     # build a dataframe with the gene sets
     padded_class_to_gene_set = pad_lists(class_to_gene_set)
@@ -67,164 +65,87 @@ def get_impacted_genes_per_class():
         print(', '.join([f'{gene}' for gene in class_to_gene_set[class_id]]))
 
 
-def GO_term_analysis(num_top_terms=3):
-    # go term files - WES v3
-    class0_biol_processes = pd.read_csv('data/class0_bio_processes.csv')
-    class0_mol_functions = pd.read_csv('data/class0_mol_functions.csv')
-    class1_biol_processes = pd.read_csv('data/class1_bio_processes.csv')
-    class1_mol_functions = pd.read_csv('data/class1_mol_functions.csv')
-    class2_biol_processes = pd.read_csv('data/class2_bio_processes.csv')
-    class2_mol_functions = pd.read_csv('data/class2_mol_functions.csv')
-    class3_biol_processes = pd.read_csv('data/class3_bio_processes.csv')
-    class3_mol_functions = pd.read_csv('data/class3_mol_functions.csv')
+def plot_GO_term_analysis(num_top_terms=3):
+    # load and process GO term files for each class
+    class_enrich = {}
+    for i in range(4):
+        biol = pd.read_csv(f'data/class{i}_bio_processes.csv')
+        mol = pd.read_csv(f'data/class{i}_mol_functions.csv')
 
-    class0_biol_processes = class0_biol_processes.sort_values(
-        by=['Fold Enrichment'], ascending=False)
-    class0_biol_processes = class0_biol_processes.iloc[:num_top_terms, :]
-    class0_mol_functions = class0_mol_functions.sort_values(
-        by=['Fold Enrichment'], ascending=False)
-    class0_mol_functions = class0_mol_functions.iloc[:num_top_terms, :]
-    class1_biol_processes = class1_biol_processes.sort_values(
-        by=['Fold Enrichment'], ascending=False)
-    class1_biol_processes = class1_biol_processes.iloc[:num_top_terms, :]
-    class1_mol_functions = class1_mol_functions.sort_values(
-        by=['Fold Enrichment'], ascending=False)
-    class1_mol_functions = class1_mol_functions.iloc[:num_top_terms, :]
-    class2_biol_processes = class2_biol_processes.sort_values(
-        by=['Fold Enrichment'], ascending=False)
-    class2_biol_processes = class2_biol_processes.iloc[:num_top_terms, :]
-    class2_mol_functions = class2_mol_functions.sort_values(
-        by=['Fold Enrichment'], ascending=False)
-    class2_mol_functions = class2_mol_functions.iloc[:num_top_terms, :]
-    class3_biol_processes = class3_biol_processes.sort_values(
-        by=['Fold Enrichment'], ascending=False)
-    class3_biol_processes = class3_biol_processes.iloc[:num_top_terms, :]
-    class3_mol_functions = class3_mol_functions.sort_values(
-        by=['Fold Enrichment'], ascending=False)
-    class3_mol_functions = class3_mol_functions.iloc[:num_top_terms, :]
-    
-    class2_enrich = pd.concat(
-        [class2_biol_processes, class2_mol_functions], axis=0)
-    class3_enrich = pd.concat(
-        [class3_biol_processes, class3_mol_functions], axis=0)
-    class0_enrich = pd.concat(
-        [class0_biol_processes, class0_mol_functions], axis=0)
-    class1_enrich = pd.concat(
-        [class1_biol_processes, class1_mol_functions], axis=0)
+        # sort and keep top terms
+        biol = biol.sort_values('Fold Enrichment', ascending=False).iloc[:num_top_terms]
+        mol = mol.sort_values('Fold Enrichment', ascending=False).iloc[:num_top_terms]
 
-    # process
-    class0_enrich['Enrichment FDR'] = class0_enrich['Enrichment FDR'].apply(
-        lambda x: -np.log10(x))
-    class1_enrich['Enrichment FDR'] = class1_enrich['Enrichment FDR'].apply(
-        lambda x: -np.log10(x))
-    class2_enrich['Enrichment FDR'] = class2_enrich['Enrichment FDR'].apply(
-        lambda x: -np.log10(x))
-    class3_enrich['Enrichment FDR'] = class3_enrich['Enrichment FDR'].apply(
-        lambda x: -np.log10(x))
-    
-    class0_enrich['Pathway'] = class0_enrich['Pathway'].apply(
-        lambda x: ' '.join(x.split()[1:]))
-    class1_enrich['Pathway'] = class1_enrich['Pathway'].apply(
-        lambda x: ' '.join(x.split()[1:]))
-    class2_enrich['Pathway'] = class2_enrich['Pathway'].apply(
-        lambda x: ' '.join(x.split()[1:]))
-    class3_enrich['Pathway'] = class3_enrich['Pathway'].apply(
-        lambda x: ' '.join(x.split()[1:]))
-    class3_enrich = class3_enrich.replace(
-        'double-strand break repair via alternative nonhomologous end joining', 
-        'double-strand break repair')
-    
+        # combine and process
+        df = pd.concat([biol, mol], axis=0)
+        df['Enrichment FDR'] = -np.log10(df['Enrichment FDR'])
+        df['Pathway'] = df['Pathway'].apply(lambda x: ' '.join(x.split()[1:]))
+
+        if i == 3:
+            df.replace('double-strand break repair via alternative nonhomologous end joining',
+                       'double-strand break repair', inplace=True)
+
+        class_enrich[i] = df
+
     plt.style.use('seaborn-v0_8-whitegrid')
-    fig, (ax0, ax1, ax2, ax3) = plt.subplots(4,1,figsize=(14,16))
+    fig, axes = plt.subplots(4, 1, figsize=(14, 16))
+    titles = ['Moderate Challenges', 'Broadly Impacted', 'Social/Behavioral', 'Mixed ASD with DD']
+    xlims = [47, 110, 105, 34]
+    x_shifts = [1.4, 2.6, 3.0, 1.0]
 
-    # Calculate scaling factor for legend
-    max_bubble_size = class0_enrich['Enrichment FDR'].max() * 32
+    max_bubble_size = class_enrich[0]['Enrichment FDR'].max() * 32
 
-    ax0.hlines(y=class0_enrich['Pathway'], xmin=0, 
-               xmax=class0_enrich['Fold Enrichment']-1.4, 
-               color='black',linewidth=2.5, alpha=0.8)
-    sns.scatterplot(x='Fold Enrichment', y='Pathway', palette='black', 
-                    color='black', 
-                    s=class0_enrich['Enrichment FDR'] * max_bubble_size, 
-                    data=class0_enrich, ax=ax0)
-    for i in range(3):
-        ax0.scatter([], [], s=(i + 1) * max_bubble_size, 
-                    c='black', label=str(i + 1))
-    ax0.legend(scatterpoints=1, labelspacing=1.1, title='-log10(FDR)', 
-               title_fontsize=23, fontsize=18, loc='upper left', 
-               bbox_to_anchor=(1, 1))
-    ax0.set_xlim([0,47])
-    ax0.set_title('Moderate Challenges', fontsize=22)
-    ax0.tick_params(labelsize=18)
-    for axis in ['top','bottom','left','right']:
-        ax0.spines[axis].set_linewidth(1.5)
-        ax0.spines[axis].set_color('black')
-    ax0.spines['top'].set_visible(False)
-    ax0.spines['right'].set_visible(False)
-    ax0.set_xlabel('')
-    ax0.set_ylabel('')
-    
-    ax1.hlines(y=class1_enrich['Pathway'], xmin=0, 
-               xmax=class1_enrich['Fold Enrichment']-2.6, color='black', 
-               linewidth=2.5, alpha=0.8)
-    sns.scatterplot(x='Fold Enrichment', y='Pathway', palette='black', 
-                    color='black', 
-                    s=class1_enrich['Enrichment FDR'] * max_bubble_size, 
-                    data=class1_enrich, ax=ax1)
-    ax1.set_xlim([0,110])
-    ax1.set_title('Broadly Impacted', fontsize=22)
-    ax1.tick_params(labelsize=18)
-    for axis in ['top','bottom','left','right']:
-        ax1.spines[axis].set_linewidth(1.5)
-        ax1.spines[axis].set_color('black')
-    ax1.spines['top'].set_visible(False)        
-    ax1.spines['right'].set_visible(False)    
-    ax1.set_xlabel('')
-    ax1.set_ylabel('')
+    for i, ax in enumerate(axes):
+        df = class_enrich[i]
 
-    ax2.hlines(y=class2_enrich['Pathway'], xmin=0, 
-               xmax=class2_enrich['Fold Enrichment']-3, color='black', 
-               linewidth=2.5, alpha=0.8)
-    sns.scatterplot(x='Fold Enrichment', y='Pathway', palette='black', 
-                    color='black', 
-                    s=class2_enrich['Enrichment FDR'] * max_bubble_size, 
-                    data=class2_enrich, ax=ax2)
-    ax2.set_title('Social/Behavioral', fontsize=22)
-    ax2.tick_params(labelsize=18)
-    ax2.set_xlim([0,105])
-    for axis in ['top','bottom','left','right']:
-        ax2.spines[axis].set_linewidth(1.5)
-        ax2.spines[axis].set_color('black')
-    ax2.spines['top'].set_visible(False)
-    ax2.spines['right'].set_visible(False)
-    ax2.set_xlabel('')
-    ax2.set_ylabel('')
+        ax.hlines(
+            y=df['Pathway'],
+            xmin=0,
+            xmax=df['Fold Enrichment'] - x_shifts[i],
+            color='black',
+            linewidth=2.5,
+            alpha=0.8
+        )
 
-    ax3.hlines(y=class3_enrich['Pathway'], xmin=0, 
-               xmax=class3_enrich['Fold Enrichment']-1, color='black', 
-               linewidth=2.5, alpha=0.8)
-    sns.scatterplot(x='Fold Enrichment', y='Pathway', palette='black', 
-                    color='black', 
-                    s=class3_enrich['Enrichment FDR'] * max_bubble_size, 
-                    data=class3_enrich, ax=ax3)
-    ax3.set_title('Mixed ASD with DD', fontsize=22)
-    ax3.set_xlim([0,34])
-    ax3.tick_params(labelsize=18)
-    for axis in ['top','bottom','left','right']:
-        ax3.spines[axis].set_linewidth(1.5)
-        ax3.spines[axis].set_color('black')
-    ax3.spines['top'].set_visible(False)
-    ax3.spines['right'].set_visible(False)
-    ax3.set_xlabel('Fold Enrichment', fontsize=24)
-    ax3.set_ylabel('')
+        sns.scatterplot(
+            x='Fold Enrichment',
+            y='Pathway',
+            data=df,
+            ax=ax,
+            color='black',
+            s=df['Enrichment FDR'] * max_bubble_size
+        )
+
+        if i == 0:
+            for j in range(3):
+                ax.scatter([], [], s=(j + 1) * max_bubble_size, c='black', label=str(j + 1))
+            ax.legend(
+                scatterpoints=1,
+                labelspacing=1.1,
+                title='-log10(FDR)',
+                title_fontsize=23,
+                fontsize=18,
+                loc='upper left',
+                bbox_to_anchor=(1, 1)
+            )
+
+        ax.set_title(titles[i], fontsize=22)
+        ax.set_xlim([0, xlims[i]])
+        ax.tick_params(labelsize=18)
+        for side in ['top', 'bottom', 'left', 'right']:
+            ax.spines[side].set_linewidth(1.5)
+            ax.spines[side].set_color('black')
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.set_xlabel('Fold Enrichment' if i == 3 else '', fontsize=24 if i == 3 else 0)
+        ax.set_ylabel('')
 
     fig.suptitle('Enrichment of pathways and processes', fontsize=26)
     fig.tight_layout()
-    plt.savefig('figures/GO_enrichment_figure.png', 
-                bbox_inches='tight', dpi=600)
+    plt.savefig('figures/GO_enrichment_figure.png', bbox_inches='tight', dpi=600)
     plt.close()
 
 
 if __name__ == '__main__':
     get_impacted_genes_per_class()
-    GO_term_analysis()
+    plot_GO_term_analysis()
