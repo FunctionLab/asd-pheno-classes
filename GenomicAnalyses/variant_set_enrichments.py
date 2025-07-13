7d81927017e0bc5866e8d8d3e4c5b22f1ce91dfa
aviya <52050744+aviyalitman@users.noreply.github.com>
2025-07-13 15:43:45 -0400

    cleanup tested variant_set_enrichments.py

--- GenomicAnalyses/variant_set_enrichments.py
+++ GenomicAnalyses/variant_set_enrichments.py
@@ -62,6 +62,7 @@ def compute_variant_set_proportions():
     props = []
     stds = []
     confidence_intervals = []
+    pvals = []
     fold_changes = []
     for gene_set in gene_sets_to_keep:
         dnvs_pro['lof_gene_set&consequence'] = dnvs_pro[gene_set] * \
@@ -109,73 +110,43 @@ def compute_variant_set_proportions():
                 zero_sibs['count'].astype(int).tolist()
         
         # compute means
-        props.append(np.sum(sibs)/num_sibs)
-        props.append(np.sum(class0)/num_class0)
-        props.append(np.sum(class1)/num_class1)
-        props.append(np.sum(class2)/num_class2)
-        props.append(np.sum(class3)/num_class3)
-
-        # compute standard errors
-        stds.append(sem(sibs))
-        stds.append(sem(class0))
-        stds.append(sem(class1))
-        stds.append(sem(class2))
-        stds.append(sem(class3))
-
-        # compute 95% confidence intervals
-        confidence_intervals.append(st.t.interval(
-            confidence=0.95, df=len(sibs)-1, loc=np.mean(sibs), scale=st.sem(sibs)))
-        confidence_intervals.append(st.t.interval(
-            confidence=0.95, df=len(class0)-1, loc=np.mean(class0), scale=st.sem(class0)))
-        confidence_intervals.append(st.t.interval(
-            confidence=0.95, df=len(class1)-1, loc=np.mean(class1), scale=st.sem(class1)))
-        confidence_intervals.append(st.t.interval(
-            confidence=0.95, df=len(class2)-1, loc=np.mean(class2), scale=st.sem(class2)))
-        confidence_intervals.append(st.t.interval(
-            confidence=0.95, df=len(class3)-1, loc=np.mean(class3), scale=st.sem(class3)))
-
-        pvals = []
-        pvals.append(ttest_ind(
-            class0, sibs, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class1, sibs, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class2, sibs, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class3, sibs, equal_var=False, alternative='greater').pvalue)
-
-        fold_changes.append((np.sum(class0)/num_class0)/(np.sum(sibs)/num_sibs))
-        fold_changes.append((np.sum(class1)/num_class1)/(np.sum(sibs)/num_sibs))
-        fold_changes.append((np.sum(class2)/num_class2)/(np.sum(sibs)/num_sibs))
-        fold_changes.append((np.sum(class3)/num_class3)/(np.sum(sibs)/num_sibs))
-
-        # additional hypothesis testing between case-groups
-        pvals.append(ttest_ind(
-            class1, class0, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class1, class2, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class1, class3, equal_var=False, alternative='greater').pvalue) 
-        pvals.append(ttest_ind(
-            class0, class2, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class3, class2, equal_var=False, alternative='greater').pvalue) 
-        pvals.append(ttest_ind(
-            class0, class3, equal_var=False, alternative='greater').pvalue) 
-
-        fold_changes.append((np.sum(class1)/num_class1)/(np.sum(class0)/num_class0))
-        fold_changes.append((np.sum(class1)/num_class1)/(np.sum(class2)/num_class2))
-        fold_changes.append((np.sum(class1)/num_class1)/(np.sum(class3)/num_class3))
-        fold_changes.append((np.sum(class0)/num_class0)/(np.sum(class2)/num_class2))
-        fold_changes.append((np.sum(class3)/num_class3)/(np.sum(class2)/num_class2))
-        fold_changes.append((np.sum(class0)/num_class0)/(np.sum(class3)/num_class3))  
-
+        groups = {
+            'sibs': sibs,
+            'class0': class0,
+            'class1': class1,
+            'class2': class2,
+            'class3': class3
+        }
+        
+        for data in groups.values():
+            props.append(np.mean(data))  # equivalent to sum / n
+            stds.append(sem(data))
+            confidence_intervals.append(
+                st.t.interval(confidence=0.95, df=len(data) - 1, loc=np.mean(data), scale=sem(data))
+            )
+    
+        classes = [class0, class1, class2, class3]
+        sizes = [num_class0, num_class1, num_class2, num_class3]
+        
+        # compare each class to sibs
+        for cls_data, cls_size in zip(classes, sizes):
+            pvals.append(ttest_ind(cls_data, sibs, equal_var=False, alternative='greater').pvalue)
+            fold_changes.append((np.sum(cls_data) / cls_size) / (np.sum(sibs) / num_sibs))
+        
+        # case-group pairwise comparisons
+        comparison_pairs = [(1, 0), (1, 2), (1, 3), (0, 2), (3, 2), (0, 3)]
+        for i, j in comparison_pairs:
+            pvals.append(ttest_ind(classes[i], classes[j], equal_var=False, alternative='greater').pvalue)
+            fold_changes.append((np.sum(classes[i]) / sizes[i]) / (np.sum(classes[j]) / sizes[j]))
+    
         uncorrected_pvals = pvals
         pvals = multipletests(pvals, method='fdr_bh')[1]
 
         # add to supp table - one row per comparison
-        index_group1 = [class_names[0], class_names[1], class_names[2], class_names[3], class_names[1], class_names[1], class_names[1], class_names[0], class_names[3], class_names[0]]
-        index_vs = ['siblings', 'siblings', 'siblings', 'siblings', class_names[0], class_names[2], class_names[3], class_names[2], class_names[2], class_names[3]]
+        index_group1 = [class_names[0], class_names[1], class_names[2], class_names[3], class_names[1], 
+                        class_names[1], class_names[1], class_names[0], class_names[3], class_names[0]]
+        index_vs = ['siblings', 'siblings', 'siblings', 'siblings', class_names[0], class_names[2], 
+                    class_names[3], class_names[2], class_names[2], class_names[3]]
         for i in range(10):
             supp_table = supp_table.append({
                 'variant type': 'dnLoF + dnMis',
@@ -187,26 +158,18 @@ def compute_variant_set_proportions():
             }, ignore_index=True)
         
         pvals = {i: pval for i, pval in enumerate(pvals)} 
-        print(pvals)
       
     fig, ax = plt.subplots(1,2,figsize=(11,4.5))
     x_values = np.arange(len(props))
     y_values = props
     colors = ['dimgray', '#FBB040', '#EE2A7B', '#39B54A', '#27AAE1']
     
-    # plot with SEM
-    #for i in range(len(x_values)):
-    #    ax[0].errorbar(
-    #        x_values[i], y_values[i], yerr=stds[i], 
-    #        fmt='o', color=colors[i], markersize=20)
-    
     # plot with 95% confidence intervals
     for i in range(len(x_values)):
-        lower_err = y_values[i] - confidence_intervals[i][0]  # Difference from lower bound
-        upper_err = confidence_intervals[i][1] - y_values[i]  # Difference from upper bound
+        lower_err = y_values[i] - confidence_intervals[i][0] 
+        upper_err = confidence_intervals[i][1] - y_values[i] 
         yerr = np.array([[lower_err], [upper_err]])
 
-        # Plot with error bars
         ax[0].errorbar(
             x_values[i], y_values[i], yerr=yerr, 
             fmt='o', color=colors[i], markersize=20)
@@ -222,22 +185,16 @@ def compute_variant_set_proportions():
     plt.subplots_adjust(top=0.95, bottom=0.1, left=0.1, right=0.95)
     for axis in ['top','bottom','left','right']:
         ax[0].spines[axis].set_linewidth(1.5)
-        # ax[0].spines[axis].set_color('black')
     ax[0].spines['top'].set_visible(False)
     ax[0].spines['right'].set_visible(False)
     ax[0].grid(color='gray', linestyle='-', linewidth=0.5)
     ax[0].set_xticklabels(['']*len(x_values))
 
-    print(f"dn pvals: {pvals}")
-
     # add significance to plot
     for grpidx in [0,1,2,3]:
         p_value = pvals[grpidx]
         x_position = grpidx+1
-        y_position = y_values[grpidx+1]
-        se_value = stds[grpidx+1]
-        #ypos = y_position + se_value - 0.001 # sem
-        ypos = confidence_intervals[grpidx+1][1] - 0.007 # 95% CI
+        ypos = confidence_intervals[grpidx+1][1] - 0.007
         if p_value < 0.01:
             ax[0].annotate('***', xy=(x_position, ypos), 
                            ha='center', size=20, fontweight='bold')
@@ -259,8 +216,6 @@ def compute_variant_set_proportions():
     star_labels = get_star_labels(custom_pvalues, custom_thresholds)
     pairs = [(1, 2), (2, 3), (2, 4)] 
     y_positions = [0.6, 0.635, 0.67] 
-
-    # Call the function to draw lines and stars
     draw_lines_and_stars(ax[0], pairs, y_positions, star_labels)
 
     # inhLoF + inhMis
@@ -309,6 +264,7 @@ def compute_variant_set_proportions():
     
     props = []
     stds = []
+    pvals = []
     confidence_intervals = []
     fold_changes = []
     for i in gene_set_indices:
@@ -337,74 +293,42 @@ def compute_variant_set_proportions():
         sibs = [sum(x) for x in zip(sibs_lof, sibs_mis)]
 
         # compute means
-        props.append(np.sum(sibs)/num_sibs)
-        props.append(np.sum(class0)/num_class0)
-        props.append(np.sum(class1)/num_class1)
-        props.append(np.sum(class2)/num_class2)
-        props.append(np.sum(class3)/num_class3)
-
-        # compute standard errors
-        stds.append(np.std(sibs)/np.sqrt(num_sibs))
-        stds.append(np.std(class0)/np.sqrt(num_class0))
-        stds.append(np.std(class1)/np.sqrt(num_class1))
-        stds.append(np.std(class2)/np.sqrt(num_class2))
-        stds.append(np.std(class3)/np.sqrt(num_class3))
-
-        # compute 95% confidence intervals
-        confidence_intervals.append(st.t.interval(
-            confidence=0.95, df=len(sibs)-1, loc=np.mean(sibs), scale=st.sem(sibs)))
-        confidence_intervals.append(st.t.interval(
-            confidence=0.95, df=len(class0)-1, loc=np.mean(class0), scale=st.sem(class0)))
-        confidence_intervals.append(st.t.interval(
-            confidence=0.95, df=len(class1)-1, loc=np.mean(class1), scale=st.sem(class1)))
-        confidence_intervals.append(st.t.interval(
-            confidence=0.95, df=len(class2)-1, loc=np.mean(class2), scale=st.sem(class2)))
-        confidence_intervals.append(st.t.interval(
-            confidence=0.95, df=len(class3)-1, loc=np.mean(class3), scale=st.sem(class3)))
-
-        # hypothesis testing
-        pvals = []
+        groups = {
+            'sibs': (sibs, num_sibs),
+            'class0': (class0, num_class0),
+            'class1': (class1, num_class1),
+            'class2': (class2, num_class2),
+            'class3': (class3, num_class3),
+        }
         
-        # siblings vs. case-groups
-        pvals.append(ttest_ind(
-            class0, sibs, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class1, sibs, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class2, sibs, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class3, sibs, equal_var=False, alternative='greater').pvalue)
-
-        fold_changes.append((np.sum(class0)/num_class0)/(np.sum(sibs)/num_sibs))
-        fold_changes.append((np.sum(class1)/num_class1)/(np.sum(sibs)/num_sibs))
-        fold_changes.append((np.sum(class2)/num_class2)/(np.sum(sibs)/num_sibs))
-        fold_changes.append((np.sum(class3)/num_class3)/(np.sum(sibs)/num_sibs))
+        for data, n in groups.values():
+            props.append(np.sum(data) / n)
+            stds.append(np.std(data) / np.sqrt(n))
+            confidence_intervals.append(
+                st.t.interval(confidence=0.95, df=len(data) - 1, loc=np.mean(data), scale=st.sem(data))
+            )
+    
+        # hypothesis testing
+        classes = [class0, class1, class2, class3]
+        sizes = [num_class0, num_class1, num_class2, num_class3]
+        
+        # class vs sibs comparisons
+        for cls_data, cls_size in zip(classes, sizes):
+            pvals.append(ttest_ind(cls_data, sibs, equal_var=False, alternative='greater').pvalue)
+            fold_changes.append((np.sum(cls_data) / cls_size) / (np.sum(sibs) / num_sibs))
         
         # case-group comparisons
-        pvals.append(ttest_ind(
-            class3, class0, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class3, class2, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class0, class2, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class3, class1, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class1, class2, equal_var=False, alternative='greater').pvalue)
-        pvals.append(ttest_ind(
-            class1, class0, equal_var=False, alternative='greater').pvalue)
-        
-        fold_changes.append((np.sum(class3)/num_class3)/(np.sum(class0)/num_class0))
-        fold_changes.append((np.sum(class3)/num_class3)/(np.sum(class2)/num_class2))
-        fold_changes.append((np.sum(class0)/num_class0)/(np.sum(class2)/num_class2))
-        fold_changes.append((np.sum(class3)/num_class3)/(np.sum(class1)/num_class1))
-        fold_changes.append((np.sum(class1)/num_class1)/(np.sum(class2)/num_class2))
-        fold_changes.append((np.sum(class1)/num_class1)/(np.sum(class0)/num_class0))
+        comparison_pairs = [(3, 0), (3, 2), (0, 2), (3, 1), (1, 2), (1, 0)]
+        for i, j in comparison_pairs:
+            pvals.append(ttest_ind(classes[i], classes[j], equal_var=False, alternative='greater').pvalue)
+            fold_changes.append((np.sum(classes[i]) / sizes[i]) / (np.sum(classes[j]) / sizes[j]))
         
         uncorrected_pvals = pvals
         pvals = multipletests(pvals, method='fdr_bh')[1]
-        group1_names = [class_names[0], class_names[1], class_names[2], class_names[3], class_names[3], class_names[3], class_names[0], class_names[3], class_names[1], class_names[1]]
-        vs_names = ['siblings', 'siblings', 'siblings', 'siblings', class_names[0], class_names[2], class_names[2], class_names[1], class_names[2], class_names[0]]
+        group1_names = [class_names[0], class_names[1], class_names[2], class_names[3], class_names[3], 
+                        class_names[3], class_names[0], class_names[3], class_names[1], class_names[1]]
+        vs_names = ['siblings', 'siblings', 'siblings', 'siblings', class_names[0], class_names[2], 
+                    class_names[2], class_names[1], class_names[2], class_names[0]]
         for i in range(10):
             supp_table = supp_table.append({
                 'variant type': 'inhLoF + inhMis',
@@ -415,20 +339,12 @@ def compute_variant_set_proportions():
                 'fold change': fold_changes[i]
             }, ignore_index=True)
         pvals = {i: pval for i, pval in enumerate(pvals)}
-        print(pvals)
-    
-    supp_table.to_csv('../supp_tables/Supp_Table_intergroup_variant_set_enrichments.csv', index=False)
-    
+        
     x_values = np.arange(len(props))
     y_values = props
     colors = ['dimgray', '#FBB040', '#EE2A7B', '#39B54A', '#27AAE1']
     
-    #for i in range(len(x_values)):
-    #    ax[1].errorbar(x_values[i], y_values[i], yerr=stds[i], 
-    #                   fmt='o', color=colors[i], markersize=20)
-    
     for i in range(len(x_values)):
-        # Get lower and upper bounds from confidence intervals
         lower_err = y_values[i] - confidence_intervals[i][0]
         upper_err = confidence_intervals[i][1] - y_values[i]
         yerr = np.array([[lower_err], [upper_err]])
@@ -442,21 +358,15 @@ def compute_variant_set_proportions():
     
     for axis in ['top','bottom','left','right']:
         ax[1].spines[axis].set_linewidth(1.5)
-        ax[1].spines[axis].set_color('black')
     ax[1].spines['top'].set_visible(False)
     ax[1].spines['right'].set_visible(False)
     ax[1].grid(color='gray', linestyle='-', linewidth=0.5)
     ax[1].set_xticklabels(['']*len(x_values))
 
-    print(f"inh pvals: {pvals}")
-
     # add significance to plot
     for grpidx in [0,1,2,3]:
         p_value = pvals[grpidx]
         x_position = grpidx+1
-        y_position = y_values[grpidx+1]
-        se_value = stds[grpidx+1]
-        #ypos = y_position + se_value-0.05
         ypos = confidence_intervals[grpidx+1][1] - 0.11
         if p_value < 0.01:
             ax[1].annotate('***', xy=(x_position, ypos), 
@@ -480,8 +390,6 @@ def compute_variant_set_proportions():
     star_labels = get_star_labels(custom_pvalues, custom_thresholds)
     pairs = [(1, 4), (3, 4), (1,3), (2,4)] 
     y_positions = [52, 52.6, 53.2, 53.8] 
-
-    # Call the function to draw lines and stars
     draw_lines_and_stars(ax[1], pairs, y_positions, star_labels)
 
     fig.tight_layout()
@@ -492,6 +400,8 @@ def compute_variant_set_proportions():
         dpi=900
         )
     plt.close()
+
+    supp_table.to_csv('../supp_tables/Supp_Table_intergroup_variant_set_enrichments.csv', index=False)
 
 
 if __name__ == "__main__":
